b969d1403e4512ff5d63ac58a8bb613203aa8a2c
Qmlnt <qmelint@gmail.com>
2023-06-09 12:13:24 +0300

    24:13:12  9/09/23

--- save.sh
+++ save.sh
@@ -1,22 +1,20 @@
 cd /home/qmlnt/Backups/dotfiles/
 
 sudo -k qncm -if list_system --to dotfiles
-printf "\nSystem done!\n"
 qncm -if list_user --to dotfiles
-printf "\nTree:"
-tree -a dotfiles | tee tree
+printf "\nTree:\n"
+tree -aC --dirsfirst dotfiles | tee tree
 # date +'%S:%M:%H %e/%d/%y' >> tree
 
 printf "\n\nChanges:\n"
 git status --short
-printf "\nAdding:\n"
 git add --all
 printf "\nAdded:\n"
 git status --short
-prinf "\nCommitting:\n"
+printf "\nCommitting:\n"
 git commit -m "`date +'%S:%M:%H %e/%d/%y'`"
 printf "\nPushing:\n"
 git push origin
-printf "\nDONE!\a"
+printf "\nDONE!\n\a"
 
 cd -
